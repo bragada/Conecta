75ecdc975af4699d2da863a3b0cb057225a1b9b0
Henrique Kanso Bragada <hkbragada@gmail.com>
2024-11-06 22:18:51 -0300

    Update upload_api_rib.R

--- upload_api_rib.R
+++ upload_api_rib.R
@@ -40,15 +40,18 @@ at_rib_extrai_json_api <- function(nome,url,raiz_1,raiz_2){
   atendimentos <- dados %>% 
     janitor::clean_names() %>% 
     select(-endereco) %>% 
-    rename(endereco = nome_logradouro,
-           lat = latitude_total_ponto,
-           lon = longitude_total_ponto,
-           equipe = desc_equipe,
-           atendimento = desc_status_atendimento_ps,
-           motivo = desc_motivo_atendimento_ps,
-           no_atendimento = id_atendimento_ps,
-           protocolo = numero_protocolo,
-           tipo_de_ocorrencia = desc_tipo_ocorrencia) %>%
+    rename(
+      equipe = any_of("desc_equipe"),
+       tipo_de_ocorrencia = any_of("desc_tipo_ocorrencia"),
+       bairro = any_of("nome_bairro"),
+       endereco = any_of("endereco_livre"),
+       protocolo = any_of("numero_protocolo"),
+       id_ordem_servico = any_of("id_ordem_servico"),
+       data_reclamacao = any_of("data_reclamacao"),
+       hora_limite_atendimento = any_of("hora_limite_atendimento"),
+       data_limite_atendimento = any_of("data_limite_atendimento"),
+       latitude_total = any_of("latitude_total"),
+       longitude_total = any_of("longitude_total")) %>%
     mutate(
       data_atendimento = as.Date(data_atendimento, "%d/%m/%Y"),
       semana_marco = week(data_atendimento) - week(as.Date("2023-02-25")),
@@ -145,16 +148,15 @@ sol_rib_extrai_json_api <- function(nome,url,raiz_1,raiz_2){
   
   solicitacoes <- dados %>% 
     clean_names() %>%
-    select(protocolo = numero_protocolo,
-           data_reclamacao,
-           status = desc_status_solicitacao,
-           tempo_restante = desc_prazo_restante,
-           data_reclamacao,
-           id_ocorrencia,
-           possui_atendimento_anterior,
-           endereco_livre_solicitacao,
-           origem_ocorrencia = desc_tipo_origem_solicitacao,
-           pontos
+    select(  
+       protocolo = any_of("numero_protocolo"),
+       status = any_of("desc_status_solicitacao"),
+       tempo_restante = any_of("desc_prazo_restante"),
+       id_ocorrencia = any_of("id_ocorrencia"),
+       possui_atendimento_anterior = any_of("possui_atendimento_anterior"),
+       endereco_livre_solicitacao = any_of("endereco_livre_solicitacao"),
+       origem_ocorrencia = any_of("desc_tipo_origem_solicitacao"),
+       pontos = any_of("pontos")
     ) %>% 
     mutate(data_reclamacao = as.Date(data_reclamacao,"%d/%m/%Y"),
            semana_marco = week(data_reclamacao)-week(as.Date("2023-02-25")),
@@ -237,12 +239,13 @@ osp_rib_extrai_json_api <- function(nome,url,raiz_1,raiz_2){
   
   osp <- dados %>% 
     clean_names() %>% 
-    select(id_ocorrencia=id_ocorrencia,
-           protocolo = numero_protocolo,
-           tipo_ocorrencia = desc_tipo_ocorrencia,
-           status = descricao_status,
-           origem_ocorrencia = desc_tipo_origem_ocorrencia,
-           prioridade = sigla_prioridade_ponto_ocorr) %>% 
+    select(
+       id_ocorrencia = any_of("id_ocorrencia"),
+       protocolo = any_of("numero_protocolo"),
+       tipo_ocorrencia = any_of("desc_tipo_ocorrencia"),
+       status = any_of("descricao_status"),
+       origem_ocorrencia = any_of("desc_tipo_origem_ocorrencia"),
+       prioridade = any_of("sigla_prioridade_ponto_ocorr")) %>% 
     distinct()
   
   
@@ -302,19 +305,19 @@ p_oc_rib_extrai_json_api_rib <- function(nome,url,raiz_1,raiz_2){
   p_oc <- dados %>% 
     clean_names() %>% 
     select(
-      protocolo = numero_protocolo ,
-      tipo_de_ocorrencia = desc_tipo_origem_ocorrencia,
-      bairro = nome_bairro,
-      endereco = endereco_livre,
-      id_ocorrencia,
-      data_reclamacao,
-      endereco_livre = nome_logradouro_completo,
-      data_limite_atendimento,
-      hora_limite_atendimento,
-      latitude_total,
-      longitude_total,
-      possui_atendimento_anterior,
-      quant_solicitacoes_vinculadas
+ protocolo = any_of("numero_protocolo"),
+    tipo_de_ocorrencia = any_of("desc_tipo_origem_ocorrencia"),
+    bairro = any_of("nome_bairro"),
+    endereco = any_of("endereco_livre"),
+    id_ocorrencia = any_of("id_ocorrencia"),
+    data_reclamacao = any_of("data_reclamacao"),
+    endereco_livre = any_of("nome_logradouro_completo"),
+    data_limite_atendimento = any_of("data_limite_atendimento"),
+    hora_limite_atendimento = any_of("hora_limite_atendimento"),
+    latitude_total = any_of("latitude_total"),
+    longitude_total = any_of("longitude_total"),
+    possui_atendimento_anterior = any_of("possui_atendimento_anterior"),
+    quant_solicitacoes_vinculadas = any_of("quant_solicitacoes_vinculadas")
     ) %>% 
     mutate(
       limite_atendimento =  as.POSIXct(strptime(paste(data_limite_atendimento,hora_limite_atendimento),"%d/%m/%Y %H:%M")),
@@ -403,17 +406,17 @@ p_moni_rib_extrai_json_api <- function(nome,url,raiz_1,raiz_2){
       longitude_total = ifelse("longitude_total" %in% names(dados),latitude_total,NA)
     ) %>% 
     select(
-      equipe = desc_equipe,
-      tipo_de_ocorrencia = desc_tipo_ocorrencia,
-      bairro = nome_bairro,
-      endereco = endereco_livre,
-      protocolo = numero_protocolo,
-      id_ordem_servico,
-      data_reclamacao,
-      hora_limite_atendimento,
-      data_limite_atendimento,
-      latitude_total,
-      longitude_total
+    equipe = any_of("desc_equipe"),
+    tipo_de_ocorrencia = any_of("desc_tipo_ocorrencia"),
+    bairro = any_of("nome_bairro"),
+    endereco = any_of("endereco_livre"),
+    protocolo = any_of("numero_protocolo"),
+    id_ordem_servico = any_of("id_ordem_servico"),
+    data_reclamacao = any_of("data_reclamacao"),
+    hora_limite_atendimento = any_of("hora_limite_atendimento"),
+    data_limite_atendimento = any_of("data_limite_atendimento"),
+    latitude_total = any_of("latitude_total"),
+    longitude_total = any_of("longitude_total")
     ) %>% 
     mutate(
       data_limite_para_atendimento = as.POSIXct(strptime(paste(data_limite_atendimento,hora_limite_atendimento),"%d/%m/%Y %H:%M")),
@@ -486,16 +489,16 @@ os_rib_extrai_json_api <- function(nome,url,raiz_1,raiz_2){
   os <- dados %>% 
     clean_names() %>%
     select(
-      id_ordem_servico,
-      data,
-      prazo = prazo_restante,
-      status = desc_status_ordem_servico,
-      desc_tipo_ordem_servico,
-      equipe = desc_equipe,
-      desc_ordem_servico,
-      data_hora_recebido,
-      total_pontos,
-      total_atendidos
+     id_ordem_servico = any_of("id_ordem_servico"),
+    data = any_of("data"),
+    prazo = any_of("prazo_restante"),
+    status = any_of("desc_status_ordem_servico"),
+    desc_tipo_ordem_servico = any_of("desc_tipo_ordem_servico"),
+    equipe = any_of("desc_equipe"),
+    desc_ordem_servico = any_of("desc_ordem_servico"),
+    data_hora_recebido = any_of("data_hora_recebido"),
+    total_pontos = any_of("total_pontos"),
+    total_atendidos = any_of("total_atendidos")
     ) %>% 
     mutate(data= as.POSIXct(strptime(data,"%d/%m/%Y %H:%M")),
            prazo = as.numeric(str_replace(prazo,",",".")),
@@ -556,13 +559,13 @@ oa_rib_extrai_json_api <- function(nome,url,raiz_1,raiz_2){
   oa <- dados %>% 
     clean_names() %>%
     select(
-      protocolo = numero_protocolo,
-      tipo_de_ocorrencia = desc_tipo_ocorrencia,
-      data_limite_de_atendimento_original = data_limite_atendimento,
-      bairro = nome_bairro,
-      endereco  = nome_logradouro_completo,
-      tempo_paralisado = tempo_pendente,
-      data_reclamacao 
+      protocolo = any_of("numero_protocolo"),
+    tipo_de_ocorrencia = any_of("desc_tipo_ocorrencia"),
+    data_limite_de_atendimento_original = any_of("data_limite_atendimento"),
+    bairro = any_of("nome_bairro"),
+    endereco = any_of("nome_logradouro_completo"),
+    tempo_paralisado = any_of("tempo_pendente"),
+    data_reclamacao = any_of("data_reclamacao")
     ) %>% 
     mutate(data_limite_de_atendimento_original = as.Date(data_limite_de_atendimento_original,"%d/%m/%Y"),
            data_reclamacao = as.Date(data_reclamacao,"%d/%m/%Y"))
@@ -621,14 +624,14 @@ sgi_rib_extrai_json_api <- function(nome,url,raiz_1,raiz_2){
   sgi <- dados %>% 
     clean_names() %>% 
     select(
-      atendimento = id_atendimento_ps,
-      prazo = data_limite_atendimento,
-      prazo_hora = hora_limite_atendimento,
-      data_atendimento,
-      atendimento_hora = hora_atendimento,
-      prev_execucao_horas = previsao_execucao,
-      status = no_prazo,
-      origem_da_ocorrencia = origem_ocorrencia
+    atendimento = any_of("id_atendimento_ps"),
+    prazo = any_of("data_limite_atendimento"),
+    prazo_hora = any_of("hora_limite_atendimento"),
+    data_atendimento = any_of("data_atendimento"),
+    atendimento_hora = any_of("hora_atendimento"),
+    prev_execucao_horas = any_of("previsao_execucao"),
+    status = any_of("no_prazo"),
+    origem_da_ocorrencia = any_of("origem_ocorrencia")
       
     ) %>% 
     #select(-x1) %>% 
@@ -732,24 +735,24 @@ mod_rib_extrai_json_api <- function(nome,url,raiz_1,raiz_2){
   mod <- dados %>% 
     clean_names() %>% 
     select(
-      etiqueta = id_ponto_servico,
-      data_mod = data_ultima_mod,                                      
-      hora = hora_ultima_mod,                                           
-      equipe = equipe_ultima_mod,                              
-      endereco,                                   
-      lat = latitude,                                      
-      lon = longitude,                                       
-      potencia_da_lampada_ultima_modernizacao = potencia_lampada_atual,
-      tipo_de_lampada_anterior = tipo_lampada_anterior,                
-      potencia_da_lampada_anterior = potencia_lampada_anterior,    
-      quantidade_ultima_modernizacao = quantidade_ultima_mod,        
-      quantidade_anterior,                                            
-      tipo_anterior,                                                 
-      desc_item_anterior,                                             
-      cod_item_anterior,                                              
-      tipo_atual,                                                     
-      desc_item_atual,                                               
-      cod_item_atual                                                  
+ etiqueta = any_of("id_ponto_servico"),
+    data_mod = any_of("data_ultima_mod"),
+    hora = any_of("hora_ultima_mod"),
+    equipe = any_of("equipe_ultima_mod"),
+    endereco = any_of("endereco"),
+    lat = any_of("latitude"),
+    lon = any_of("longitude"),
+    potencia_da_lampada_ultima_modernizacao = any_of("potencia_lampada_atual"),
+    tipo_de_lampada_anterior = any_of("tipo_lampada_anterior"),
+    potencia_da_lampada_anterior = any_of("potencia_lampada_anterior"),
+    quantidade_ultima_modernizacao = any_of("quantidade_ultima_mod"),
+    quantidade_anterior = any_of("quantidade_anterior"),
+    tipo_anterior = any_of("tipo_anterior"),
+    desc_item_anterior = any_of("desc_item_anterior"),
+    cod_item_anterior = any_of("cod_item_anterior"),
+    tipo_atual = any_of("tipo_atual"),
+    desc_item_atual = any_of("desc_item_atual"),
+    cod_item_atual = any_of("cod_item_atual")                                                
     ) %>% 
     mutate(
       data_mod = as.Date(data_mod,"%d/%m/%Y"),
