d2c08af08295adcabc34da3e0f8ce69dba8ab38f
Henrique Kanso Bragada <hkbragada@gmail.com>
2024-12-01 22:23:44 -0300

    Update teste_lib.R

--- teste_lib.R
+++ teste_lib.R
@@ -137,7 +137,7 @@ sol_extrai_json_api <- function(nome,url,raiz_1,raiz_2){
     
     corpo_requisicao <- list(
         CMD_ID_STATUS_SOLICITACAO = -1,
-        CMD_ID_PARQUE_SERVICO = "1,2",
+        CMD_IDS_PARQUE_SERVICO = "1,2",
         CMD_DATA_RECLAMACAO = "01/03/2023",
         CMD_APENAS_EM_ABERTO = 0
     )
