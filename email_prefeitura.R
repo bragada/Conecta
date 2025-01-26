f8e461666012afe45320f5cd358f030f6aff1565
Henrique Kanso Bragada <hkbragada@gmail.com>
2025-01-26 14:31:49 -0300

    Create email_prefeitura.R

---
+++ email_prefeitura.R
@@ -0,0 +1,43 @@
+pacman::p_load(aws.s3,googledrive,googlesheets4,tidyverse,gmailr,rmarkdown,blastula,keyring)
+
+
+render_relatorio <- rmarkdown::render(
+  input = "script_relatorio.Rmd",
+  output_file = "program_conecta_campinas.pdf"
+)
+
+
+info_relatorio <- file.info("program_conecta_campinas.pdf")
+
+if(as.Date(info_relatorio$mtime,tz = "America/Sao_Paulo") == Sys.Date()){
+
+  email <- compose_email(
+    body = md("
+  Bom dia,  
+
+  Segue em anexo a programação diária das manutenções e modernizações previstas para a cidade de Campinas.  
+
+  Bot - HK CONSULTORIA
+  ")
+  )
+  
+  email <- email %>%
+    add_attachment(
+      file = "script_relatorio.pdf",  # Nome do arquivo que será anexado
+      content_type = "application/pdf"  # Tipo de arquivo anexado
+    )
+  
+  destinatarios <- c("rikibragada@gmail.com")
+  
+  # 4. Envio do email com as credenciais armazenadas
+  email %>%
+    smtp_send(
+      from = "hkbragada@gmail.com",
+      to = destinatarios,  
+      subject = "Programação Conecta - Prefeitura",  
+      credentials = creds_key(id = "gmail") 
+    )
+
+} else {
+  
+}
