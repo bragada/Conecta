a9c88f3d015db2f5d848d3021acd9da0fc599ae5
Henrique Kanso Bragada <hkbragada@gmail.com>
2025-02-09 17:34:26 -0300

    Update email_prefeitura.R

--- email_prefeitura.R
+++ email_prefeitura.R
@@ -1,6 +1,8 @@
-install.packages("blastula")
-install.packages("rmarkdown")
-install.packages("keyring")
+
+if (!requireNamespace("emayili", quietly = TRUE)) install.packages("emayili")
+if (!requireNamespace("keyring", quietly = TRUE)) install.packages("keyring")
+if (!requireNamespace("rmarkdown", quietly = TRUE)) install.packages("rmarkdown")
+if (!requireNamespace("blastula", quietly = TRUE)) install.packages("blastula")
 
 library(keyring)
 library(tidyverse)
@@ -9,10 +11,7 @@ library(blastula)
 
 Sys.getenv("GMAIL_AUTH")
 
-SMTP_USER <- Sys.getenv("SMTP_USER")
-SMTP_PASS <- Sys.getenv("SMTP_PASS")
-print(Sys.getenv("SMTP_USER"))
-print(Sys.getenv("SMTP_PASS"))
+
 
 rmarkdown::pandoc_version()
 render_relatorio <- rmarkdown::render(
@@ -20,41 +19,35 @@ render_relatorio <- rmarkdown::render(
   output_file = "program_conecta_campinas.pdf"
 )
 
+
 info_relatorio <- file.info("program_conecta_campinas.pdf")
 
 if(as.Date(info_relatorio$mtime,tz = "America/Sao_Paulo") == Sys.Date()){
 
-  email <- compose_email(
-    body = md("
-  Bom dia,  
+  
+
+smtp <- server(
+  host = "smtp.gmail.com",
+  port = 587,
+  username = Sys.getenv("SMTP_USER"),
+  password = Sys.getenv("SMTP_PASS")
+)
+
+# Criar o email e anexar o arquivo temporário
+email <- envelope() %>%
+  from("rikibragada@gmail.com") %>%
+  to("rikibragada@gmail.com") %>%
+  subject("Programação Conecta - Prefeitura") %>%
+  text("Bom dia,  
 
   Segue em anexo a programação diária das manutenções e modernizações previstas para a cidade de Campinas.  
 
-  Bot - HK CONSULTORIA
-  ")
-  )
-  
-  email <- email %>%
-    add_attachment(
-      file = "program_conecta_campinas.pdf",  # Nome do arquivo que será anexado
-      content_type = "application/pdf"  # Tipo de arquivo anexado
-    )
-  
-  destinatarios <- c("rikibragada@gmail.com")
-  # 4. Envio do email com as credenciais armazenadas
-  smtp_send(
-    email = email,
-    from = SMTP_USER,
-    to = destinatarios,
-    subject = "Programação Conecta - Prefeitura",
-    credentials = creds_envvar(
-      user = SMTP_USER,
-      pass_envvar = "SMTP_PASS",
-      host = "smtp.gmail.com",
-      port = 587,
-      use_ssl = TRUE
-    )
-  )
+  Bot - HK CONSULTORIA") %>%
+  attachment(path = "program_conecta_campinas.pdf")
+
+# Enviar o email
+smtp(email, verbose = TRUE)
+
 
 } else {
   
