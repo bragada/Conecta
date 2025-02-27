409a5a40bedd7b69621525aea9cfba6544f0a88b
Henrique Kanso Bragada <hkbragada@gmail.com>
2025-02-26 22:15:11 -0300

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,13 +1,20 @@
-chave_api = "asfagasg"
-
-from sendgrid = SendGridAPIClient(chave_api)
+import os
+from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-conta_sendgrid  = SendGridAPIClient(chave_api)
+# Pegando a chave da variável de ambiente
+chave_api = os.environ.get("EMAIL_API")
 
+# Criando a conta do SendGrid
+conta_sendgrid = SendGridAPIClient(chave_api)
+
+# Criando o e-mail
 email = Mail(
-              from_email = "hkbragada@gmail.com",
-              to_emails = "rikibragada@gmail.com",
-              subject = "Fala dodóizin" 
-              html_content = "<p> ó o cuzin </>
-              
+    from_email="hkbragada@gmail.com",
+    to_emails="rikibragada@gmail.com",
+    subject="Fala dodóizin",
+    html_content="<p>Ó o cuzin</p>"
+)
+
+# Enviando o e-mail
+response = conta_sendgrid.send(email
